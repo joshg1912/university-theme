afc8b9e66a82b898a050df3d06ff46e030070e38
Josh G <joshgarcia7410@gmail.com>
2022-12-12 19:45:50 -0800

    adds has archive and single-event file

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -2,6 +2,7 @@
 
 function university_post_types(){
     register_post_type('event', array(
+        'has_archive' => true,
         'public' => true,
         'show_in_rest' => true,
         'labels' => array(
@@ -10,7 +11,6 @@ function university_post_types(){
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
             'singular_name' => 'Event'
-
         ),
         'menu_icon' => 'dashicons-calendar'
         
